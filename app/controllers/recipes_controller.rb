dc1c6f1d986a9f0b06945e25f78ebffa264cb0d0
Andrew Gilbert <andrew-gilbert1@hotmail.co.uk>
2017-11-20 15:57:14 +0000

    added controller, routes, views for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,51 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+   @recipe = Recipe.new
+  end
+
+  def create
+   @recipe = Recipe.new(recipe_params)
+     if @recipe.save
+      redirect_to dashboard_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+   @recipe.update
+   redirect_to recipe_path(@recipe)
+  end
+
+  def show
+
+  end
+
+  def destroy
+   @recipe.destroy
+   redirect_to dashboard_path
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+   params.require(:recipe).permit(:name, :description, :duration, :cuisine, :servings, :photo)
+  end
 end
+
+
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
+  has_many :ingredients
   belongs_to :user
 end
