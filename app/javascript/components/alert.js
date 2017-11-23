cc9a22e1e69720ad10ec2707beda07d5e99dfeb6
Sophie Dermaux <sophiebcdermaux@gmail.com>
2017-11-23 10:50:48 +0000

    fixed sweet alert

--- app/javascript/components/alert.js
+++ app/javascript/components/alert.js
@@ -1,13 +1,13 @@
-// import swal from 'sweetalert';
+import swal from 'sweetalert';
 
-// function bindSweetAlertButtonDemo() {
-//   document.getElementById('sweet-alert-demo').addEventListener('click', () => {
-//     swal({
-//       title: "A nice alert",
-//       text: "This is a great alert, isn't it?",
-//       icon: "success"
-//     })
-//   });
-// }
+function bindSweetAlertButtonDemo() {
+  document.getElementById('sweet-alert-demo').addEventListener('click', () => {
+    swal({
+      title: "A nice alert",
+      text: "This is a great alert, isn't it?",
+      icon: "success"
+    })
+  });
+}
 
-// export { bindSweetAlertButtonDemo };
+export { bindSweetAlertButtonDemo };
